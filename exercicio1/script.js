ef71337214c8e0a9abd6f134345ec0c9d37603c5
TulioLeitao <tulioleitao@gmail.com>
2022-07-21 18:00:01 -0300

    exercício a008 operadores

---
+++ exercicio1/script.js
@@ -0,0 +1,12 @@
+let numeroPar = Number(prompt("Por favor, insira um número par"));
+
+numeroPar = numeroPar %2;
+
+console.log (numeroPar);
+
+//o resto da divisão de qualquer número par sempre retorna 0
+
+/*ao colocar um número impar o programa retorna o valor da sobra da divisão, 1, 
+como estamos divindo por 2, sempre que colocarmos um valor impar
+o valor que o programa retorna é sempre 1,
+ já que todo número real inteiro sobra 1 */
